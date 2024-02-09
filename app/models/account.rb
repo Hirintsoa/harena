475cc4bef361937b76fb687413249bde51f79c01
Hirintsoa <hirintsoa23@protonmail.com>
2024-02-09 06:47:12 +0300

    build: setup noticed

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 class Account < ApplicationRecord
   has_many :activity_participants
   has_many :activities, through: :activity_participants
+  has_many :notifications, as: :recipient, dependent: :destroy, class_name: 'Noticed::Notification'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
---
+++ app/notifiers/new_transaction_notifier.rb
@@ -0,0 +1,38 @@
+# To deliver this notification:
+#
+# NewTransaction.with(record: @post, message: "New post").deliver(User.all)
+
+class NewTransactionNotifier < ApplicationNotifier
+  deliver_by :action_cable do |config|
+    config.channel = 'TransactionChannel'
+    config.message = ->{ params.merge(user_id: recipient.id, notifications: recipient.notifications) }
+  end
+  
+  # deliver_by :email do |config|
+  #   config.mailer = "AccountMailer"
+  #   config.method = "new_transaction"
+  #   config.if = ->(recipient) { recipient.mail_notifications? }
+  #   config.wait = 1.hour
+  # end
+  #
+  # bulk_deliver_by :slack do |config|
+  #   config.url = -> { Rails.application.credentials.slack_webhook_url }
+  # end
+  #
+  # deliver_by :custom do |config|
+  #   config.class = "MyDeliveryMethod"
+  # end
+
+  # Add required params
+  #
+  # required_param :message
+  notification_methods do
+    def message
+      "New transaction initiated by #{params[:initiator]} on #{self.created_at}"
+    end
+
+    def url
+      activity_path self.record
+    end
+  end
+end
